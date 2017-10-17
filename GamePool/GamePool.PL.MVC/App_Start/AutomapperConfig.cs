662cb4e0556c953b31c3d8a2bae5699035205f7c
Ruslan <qwertysakkal@gmail.com>
2017-10-17 23:50:11 +0400

    Added automapper config class. Changed default controller in route config. Added ProductController with Index view.

---
+++ GamePool/GamePool.PL.MVC/App_Start/AutomapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamePool.PL.MVC.App_Start
+{
+    public class AutomapperConfig
+    {
+        public static void RegisterMaps()
+        {
+            Mapper.Initialize(cfg =>
+            {
+
+            });
+        }
+    }
+}
---
+++ GamePool/GamePool.PL.MVC/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using GamePool.BLL.LogicContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamePool.PL.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IGameLogic gameLogic;
+
+        public ProductController(IGameLogic gameLogic)
+        {
+            this.gameLogic = gameLogic;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
