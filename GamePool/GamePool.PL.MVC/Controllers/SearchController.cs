03545418e961158acb494400fa8c4211c853dd51
Ruslan <qwertysakkal@gmail.com>
2017-11-26 19:28:22 +0400

    Added Search controller with SearchParameterVM model and view

---
+++ GamePool/GamePool.PL.MVC/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GamePool.PL.MVC.Models.Search;
+
+namespace GamePool.PL.MVC.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index(SearchParametersVM parameters)
+        {
+            return View(parameters);
+        }
+    }
+}
---
+++ GamePool/GamePool.PL.MVC/Models/Search/SearchParametersVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GamePool.PL.MVC.Models.Search
+{
+    public class SearchParametersVM
+    {
+        [Display(Name = "Name:")]
+        public string GameName { get; set; }
+
+        [Display(Name = "Price from:")]
+        public int? PriceFrom { get; set; }
+
+        [Display(Name = "Price to:")]
+        public int? PriceTo { get; set; }
+
+        [Display(Name = "Release date:")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Genres:")]
+        public IEnumerable<int> GenreIds { get; set; }
+
+        [Required]
+        public int Page { get; set; }
+    }
+}
