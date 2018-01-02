c7608b0ce39fa46534e218257a138cd3d8e54671
Ruslan <qwertysakkal@gmail.com>
2018-01-03 02:26:37 +0400

    Added ExceptionFilter and registered it in Global.asax

---
+++ GamePool/GamePool.PL.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using GamePool.PL.MVC.Infrastructure;
+
+namespace GamePool.PL.MVC.App_Start
+{
+    public static class FilterConfig
+    {
+        public static void RegisterFilters(GlobalFilterCollection filterCollection)
+        {
+            filterCollection.Add(new ExceptionFilter());
+        }
+    }
+}
